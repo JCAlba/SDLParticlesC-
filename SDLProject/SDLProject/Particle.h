3968ce51147ce9035e7effedbdb0eeebceb68adc
Juan Albahaca <juanalbahaca92@gmail.com>
2017-12-21 20:39:19 +0100

    Particle simulation with random initialisation

--- SDLProject/SDLProject/Particle.h
+++ SDLProject/SDLProject/Particle.h
@@ -16,28 +16,46 @@ namespace jca {
     struct Particle {
         double m_x;
         double m_y;
+        
+    private:
         double m_speed;
         double m_direction;
+    private:
+        void init();
     public:
         Particle();
         virtual~Particle();
         void update(int interval);
     };
     Particle::Particle(): m_x(0), m_y(0) {
+        init();
+    }
+    
+    void Particle::init() {
+        m_x = 0;
+        m_y = 0;
         m_direction = (2*M_PI*rand())/RAND_MAX;
         m_speed = (0.04 *rand())/RAND_MAX;
-        
         m_speed *= m_speed;
     }
     Particle::~Particle() {
         
     }
     void Particle::update(int interval) {
+        m_direction += interval* .0004;
+        
         double xspeed = m_speed * cos(m_direction);
         double yspeed = m_speed * sin(m_direction);
         
         m_x += xspeed*interval;
         m_y += yspeed*interval;
+        
+        if(m_x <-1 || m_x > 1 || m_y < -1 || m_y > 1) {
+            init();
+        }
+        if(rand() < RAND_MAX/100) {
+            init();
+        }
     }
 }
 
